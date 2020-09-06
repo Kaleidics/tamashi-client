c205ba3d3da738a5ea94e1f2202892cb1a59eea9
Eddie Chu <contact@eddiechu.me>
2020-09-06 00:42:38 -0700

    add mobile menu styles

--- src/components/elements/NavBar.js
+++ src/components/elements/NavBar.js
@@ -7,7 +7,40 @@ import SignOutBtn from './SignOutBtn';
 import logofinal from '../../assets/logofinal.png';
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+           menu: false
+        };
+
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.controlMenuOpen.bind(this));
+    }
+
+    controlMenuOpen() {
+        if(window.innerWidth < 1024) {
+            this.setState({
+                menu: false
+            });
+        }
+    }
+    
+    toggleMenu = () => {
+        this.setState({
+            menu: !this.state.menu
+        });
+    }
+
     render() {
+
+
+        let menuOpenLineA = this.state.menu ? 'lineA' : '';
+        let menuOpenLineB = this.state.menu ? 'lineB' : '';
+        let menuOpenContainer = this.state.menu ? '' : 'toggleMenu';
+
         return (
             <nav className='navbar'>
                 <div className='navbar__container'>
@@ -18,19 +51,24 @@ class NavBar extends Component {
                     {this.props.auth && this.props.location.pathname === '/dashboard' ? (
                         <SignOutBtn />
                     ) : (
-                        <div className='navbar__btn-container'>
-                            {/* <Link to='/signin' className='btn mr-15'>
+                        <div className= {`navbar__btn-container ${menuOpenContainer}`}>
+                            <Link to='/signin' className='btn mobile-mtb-10 desktop-mr-15'>
                                 {this.props.auth ? 'Dashboard' : 'Sign in'}
-                            </Link> */}
+                            </Link>
                             {this.props.auth ? (
                                 <SignOutBtn />
                             ) : (
-                                <Link to='/register' className='btn btn--alt'>
+                                <Link to='/register' className='btn btn--alt mobile-mtb-10'>
                                     Register
                                 </Link>
                             )}
                         </div>
                     )}
+                    <button className='navbar__toggle' onClick={this.toggleMenu}>
+                        <span className={`navbar__toggle-line ${menuOpenLineA}`}></span>
+                        <span className={`navbar__toggle-line ${menuOpenLineB}`}></span>
+                        <p class='navbar__AX-name'>Menu</p>
+                    </button>
                 </div>
             </nav>
         );
